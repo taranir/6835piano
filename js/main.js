e7de06e2a0eb8b9e453c6861da1ecf69d00a610a
Dongfang (Tian) Mi <tianm@mit.edu>
2015-04-14 20:15:51 -0400

    initial setup

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Setup Leap loop with frame callback function
+var controllerOptions = {enableGestures: true};
+
+Leap.loop(controllerOptions, function(frame) {
+  // Body of callback function
+})
