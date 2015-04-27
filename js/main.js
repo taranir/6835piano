202ccb3a7e6d27a0e182af35d40660b9f5dda0ae
Dongfang (Tian) Mi <tianm@mit.edu>
2015-04-27 01:48:58 -0400

    tweaking static threshold

--- js/main.js
+++ js/main.js
@@ -17,47 +17,14 @@ Leap.loop(controllerOptions, function(frame) {
       CURRENT_HAND_POSITION = frame.hands[0].screenPosition()[1];
     }
     
-    handsView.setFingers(sortedFingers);
-    hoverView.setFingers(sortedFingers);
-
-    handsView.processFingers(sortedFingers);
+    handsView.setFingers(sortedFingers); //set hand visualization
+    if (CURRENT_MODE == MODES.STATIC_THRESHOLD) {
+      hoverView.setFingers(sortedFingers);
+    }
     
 
-    // //filter out only keyTap gestures
-    // var gestures = _.filter(frame.gestures, function(gesture) {
-    //   return (gesture.type == "keyTap");
-    // });
-    // // if (gestures.length > 0) {
-    // //   console.log(gestures.length + " gestures");
-    // // }
-    // if (prevGestures) {
-    //   //look for prev gestures that no longer exist
-    //   var endedGestures = _.difference(prevGestures, gestures);
-    //   if (endedGestures.length > 0) {
-    //     // console.log("ended Gestures:");
-    //     // console.log(endedGestures);
-    //     _.each(endedGestures, function(gesture) {
-    //         // console.log("keyTapEnd, id: " + gesture.id + ", duration: " + gesture.duration);
-    //         // console.log(gesture);
-    //         Backbone.trigger("keyTapEnd", gesture);
-    //         Backbone.trigger("keyTapEnd", gesture);
-    //     }); 
-    //   }
-
-
-    //   //look for new gestures that aren't in prev gestures
-    //   var startedGestures = _.difference(gestures, prevGestures);
-    //   if (startedGestures.length > 0) {
-    //     _.each(startedGestures, function(gesture) {
-    //         //console.log("keyTapStart, id: " + gesture.id);
-    //         Backbone.trigger("keyTapStart", gesture);
-    //         Backbone.trigger("keyTapStart", gesture);
-    //     });
-    //   }
-
-    // }
-
-    // prevGestures = gestures;
+    handsView.processFingers(sortedFingers); //check for keypresses
+    
 
   }
 
